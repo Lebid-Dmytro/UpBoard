1c902c5c958174983e95879f681b2c049cf82a02
Lebid_Dmytro <lebedd99@gmail.com>
2025-02-24 17:42:05 +0200

    deleted the comment

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()  # Завантажує .env змінні
+load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
